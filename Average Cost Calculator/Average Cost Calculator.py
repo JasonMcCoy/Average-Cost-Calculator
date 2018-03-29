0fd21d55a71979115a43bf503548e7d07ae41973
Jason McCoy <JasonMcCoy@yahoo.com>
2018-03-29 10:58:46 -0700

    Easier to understand output.
    - I ask users to 'enter your target LTC $ Price'.
    - After typing in targeted price to break-even, I now made it easier to understand the results being outputted.

--- Average Cost Calculator/Average Cost Calculator.py
+++ Average Cost Calculator/Average Cost Calculator.py
@@ -1,12 +1,11 @@
 '''
 Here is a Average Cost Calculator which takes your current crypto coin purchases and produces an average cost.
-
 Inspired from this website:
 http://www.bullsandthebears.com/vipmember/Average-Down-Calculator.html
 '''
 
 OriginalShares = 9.5396
-OriginalShares2 = 18.8
+OriginalShares2 = 20  # How much LTC do you plan on purchasing?
 PurchasePrice = 187.17
 PurchasePrice2 = int(input("Please enter your target LTC $ Price: "))
 
@@ -14,4 +13,5 @@ PurchasePrice2 = int(input("Please enter your target LTC $ Price: "))
 
 AveragedPricePerShare = ((OriginalShares * PurchasePrice) + (OriginalShares2 * PurchasePrice2)) / (OriginalShares + OriginalShares2)
 
-print(AveragedPricePerShare)
+
+print("Assuming you originally bought {0} LTC at ${1} and plan on buying '{2}' more LTC, \nif you buy at the price inputted, you must sell your LTC at: ${3} to break-even:".format(OriginalShares, PurchasePrice, OriginalShares2, AveragedPricePerShare))
