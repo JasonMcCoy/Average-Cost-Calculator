d0b82af20e2e1edf2154feba7568d974cbaa8b3f
Jason McCoy <JasonMcCoy@yahoo.com>
2018-03-30 12:47:51 -0700

    Now ask user questions in order to better help determine breakeven price
    How much $ will you be investing?
    What price do you plan on buying LTC?
    How much LTC did you originally buy?
    What was the price of the LTC you originally bought?
    
    Example Answer:
    Assuming you originally bought 9.5396 LTC at $287.17 and plan on buying '21.66565560273854' more LTC,
    if you buy at the price inputted, you must sell your LTC at: $167.90399023490735 to break-even:

--- Average Cost Calculator/Average Cost Calculator.py
+++ Average Cost Calculator/Average Cost Calculator.py
@@ -4,14 +4,19 @@ Inspired from this website:
 http://www.bullsandthebears.com/vipmember/Average-Down-Calculator.html
 '''
 
-OriginalShares = 9.5396
-OriginalShares2 = 20  # How much LTC do you plan on purchasing?
-PurchasePrice = 187.17
-PurchasePrice2 = int(input("Please enter your target LTC $ Price: "))
+x = float(input("How much $ will you be investing?:"))
+currentLtcPrice = float(input("What price do you plan on buying LTC?:"))
+y = float(input("How much LTC did you originally buy?:"))
+z = float(input("What was the price of the LTC you originally bought?:"))
+
+OriginalShares = y
+OriginalShares2 = x/currentLtcPrice  # How much LTC do you plan on purchasing?
+PurchasePrice = z
+PurchasePrice2 = currentLtcPrice
 
 
 
 AveragedPricePerShare = ((OriginalShares * PurchasePrice) + (OriginalShares2 * PurchasePrice2)) / (OriginalShares + OriginalShares2)
 
 
-print("Assuming you originally bought {0} LTC at ${1} and plan on buying '{2}' more LTC, \nif you buy at the price inputted, you must sell your LTC at: ${3} to break-even:".format(OriginalShares, PurchasePrice, OriginalShares2, AveragedPricePerShare))
+print("Assuming you originally bought {0} LTC at ${1} and plan on buying '{2}' more LTC, \nif you buy at the price inputted, you must sell your LTC at: ${3} to break-even.".format(OriginalShares, PurchasePrice, OriginalShares2, AveragedPricePerShare))
